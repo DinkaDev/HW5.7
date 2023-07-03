d614a9d66a11202e086d6970551deaf317225c11
Dinkalucky <burlaka.dina@gmail.com>
2023-07-03 16:00:30 +0300

    Добавьте файлы проекта.

---
+++ HW5.6/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using HW5._6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HW5._6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new FormModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormModel model)
+        {
+            Debug.WriteLine("First: " + model.First);
+            Debug.WriteLine("Second: " + model.Second);
+            Debug.WriteLine("Count: " + model.Count);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HW5.6/Models/FormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HW5._6.Models
+{
+    public class FormModel
+    {
+        public string First { get; set; }
+        public string Second { get; set; }
+        public int Count { get; set; }
+    }
+}
